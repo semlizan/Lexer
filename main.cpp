36ed89d0fc4a36b5042ac1f5bb464ead84f81d9c
Liza <semlizan@mail.ru>
2016-02-01 19:45:12 +1000

    Создание ассоциативного массива

--- main.cpp
+++ main.cpp
@@ -4,8 +4,45 @@
 #include <string>
 #include <string.h>
 #include <fstream>
+#include <map>
 
 using namespace std;
+
+enum identType {IDENT= 0, KEYWORDS, OP};
+map<string, identType> ident_type;
+void initmap() {
+	ident_type["begin"] = KEYWORDS;
+    ident_type["forward"] = KEYWORDS;
+    ident_type["do"] = KEYWORDS;
+    ident_type["else"] = KEYWORDS;
+    ident_type["end"] = KEYWORDS;
+    ident_type["for"] = KEYWORDS;
+    ident_type["function"] = KEYWORDS;
+    ident_type["if"] = KEYWORDS;
+    ident_type["array"] = KEYWORDS;
+    ident_type["of"] = KEYWORDS;
+    ident_type["procedure"] = KEYWORDS;
+    ident_type["program"] = KEYWORDS;
+    ident_type["record"] = KEYWORDS;
+    ident_type["then"] = KEYWORDS;
+    ident_type["to"] = KEYWORDS;
+    ident_type["type"] = KEYWORDS;
+    ident_type["var"] = KEYWORDS;
+    ident_type["while"] = KEYWORDS;
+    ident_type["break"] = KEYWORDS;
+    ident_type["continue"] = KEYWORDS;
+    ident_type["downto"] = KEYWORDS;
+    ident_type["exit"] = KEYWORDS;
+    ident_type["repeat"] = KEYWORDS;
+    ident_type["until"] = KEYWORDS;
+
+	ident_type["and"] = OP;
+    ident_type["div"]= OP;
+    ident_type["mod"] = OP;
+    ident_type["not"] = OP;
+    ident_type["or"] = OP;
+    ident_type["xor"] = OP;
+	}
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
@@ -73,7 +110,9 @@ Token *get_token ()
 				next_char();	
 			}
 		 }	
-   Token *token = new Token(lineCur, columnCur, tokenType, leksema);
+	if (tokenType == "ident" && (ident_type[leksema] == KEYWORDS)) tokenType = "keyword";
+    if (tokenType == "ident" && (ident_type[leksema] == OP)) tokenType = "op";
+	Token *token = new Token(lineCur, columnCur, tokenType, leksema);
         return token;
 	  
 	 }
@@ -85,7 +124,8 @@ Token *get_token ()
 }
 
 int main()
-{
+{	
+	   initmap();
     fin >> noskipws;
 	 next_char();
 	Token *cur;
